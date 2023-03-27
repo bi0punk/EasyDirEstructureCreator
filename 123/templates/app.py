6168a6cce3191526ad9bd9f712082b253ca12c4d
sysbot <systecinfo_cl@criptext.com>
2023-03-26 23:05:41 -0300

    fixes

--- 123/templates/app.py
+++ 123/templates/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
+
+
 app = Flask(__name__)
+
+
+
 @app.route("/")
 def hello_world():
    return "<p>Hello World from FasT-FlasK!, generated {{fecha_genera}}</p>"
--- app.py
+++ app.py
@@ -82,6 +82,8 @@ def crea_directorio(nombre):
     f.write('\n')
     f.write("app = Flask(__name__)\n")
     f.write('\n')
+    f.write('\n')
+    f.write('\n')
     f.write('@app.route("/")\n')
     f.write("def hello_world():\n")
     f.write('   return "<p>Hello World from FasT-FlasK!, generated {{fecha_genera}}</p>"')
