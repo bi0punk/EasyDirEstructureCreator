44fba177a5ad409b681675103d819b93fcbaf8c3
Henry <74167958+bi0punk@users.noreply.github.com>
2023-07-11 00:04:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from art import text2art
 import os
 import datetime
+from flask import Flask
 
 Art = text2art("<FAST-FLASK>")
 print(Art)
 
 def mostrar_menu(opciones):
     print('Seleccione una opción / Select an option:\n')
-    for clave in sorted(opciones):
-        print(f'{clave}) {opciones[clave][0]}')
+    for clave, valor in opciones.items():
+        print(f'{clave}) {valor[0]}')
 
 def leer_opcion(opciones):
     while True:
@@ -17,14 +18,34 @@ def leer_opcion(opciones):
             return a
         print('ERROR.')
 
-def ejecutar_opcion(opcion, opciones):
-    opciones[opcion][1]()
+def accion1():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_world():
+        fecha_genera = datetime.datetime.now()
+        return f'<p>Hello World from FasT-FlasK!, Generated: {fecha_genera}</p>'
+
+    app.run()
+
+def accion2():
+    print('Ejecutando acción 2')
+
+def accion3():
+    print('Ejecutando acción 3')
+
+def salir():
+    print('Saliendo...')
+    exit()
+
+def ayuda():
+    print('Mostrando ayuda...')
 
 def generar_menu(opciones, opcion_salida):
     while True:
         mostrar_menu(opciones)
         opcion = leer_opcion(opciones)
-        ejecutar_opcion(opcion, opciones)
+        opciones[opcion][1]()
         print()
         if opcion == opcion_salida:
             break
@@ -39,27 +60,5 @@ def menu_principal():
     }
     generar_menu(opciones, '5')
 
-class Flask_main:
-    def __init__(self, filename, ruta, contenido):
-        self.filenama = filename
-        self.ruta = ruta
-        contenido = contenido
-        mainpy = Flask_main(filename='app.py', ruta=None, contenido='')
-
-def crear_archivo(nombre, contenido):
-    ruta = f'{nombre}/templates/'
-    os.makedirs(ruta, exist_ok=True)
-    fecha_genera = datetime.datetime.now()
-
-    with open(f'{nombre}/app.py', 'w') as f:
-        f.write("from flask import Flask\n\n")
-        f.write("app = Flask(__name__)\n\n")
-        f.write("@app.route('/')\n")
-        f.write("def hello_world():\n")
-        f.write(f"    return '<p>Hello World from FasT-FlasK!, Generated: {fecha_genera} </p>'\n")
-
-    with open(f'{ruta}index.html', 'w') as f:
-        f.write("<!DOCTYPE html>\n")
-        f.write('<html lang="en">\n')
-        f.write("<head>\n")
-        f.write("<meta charset='UTF-
+if __name__ == "__main__":
+    menu_principal()
